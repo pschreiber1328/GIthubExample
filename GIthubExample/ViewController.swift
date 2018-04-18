1e082b6d247ad84574fe06c9fb96d760f118b342
Pschreiber1328 <pschreiber1328@jh-mba158-23.d214.org>
2018-04-18 14:44:19 -0500

    Initial Commit

---
+++ GIthubExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GIthubExample
+//
+//  Created by Pschreiber1328 on 4/18/18.
+//  Copyright © 2018 Pschreiber1328. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
